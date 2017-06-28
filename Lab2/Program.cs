7df4e68abe73af8fabc1cd88726cf9fb3d716038
ariana.waller@yahoo.com <ariana.waller@yahoo.com>
2017-06-28 02:12:27 -0400

    initial commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*This Program will allow the user to enter a number in between 1 and 100 and display 
+  associated result based on the integer range enetered*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User! What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + userName + "!");
+            Console.WriteLine("Welcome to the decision making process challenge");
+
+            Console.WriteLine("Please enter a number in between 1 and 100");
+            string userInput = Console.ReadLine();
+
+            if
+
+
+              
+
+
+    
+        }
+    }
+}
